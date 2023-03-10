d0cff100f4a326858a7b94a42af89f2d6385fc6a
Akshaydee <50842275+Akshaydee@users.noreply.github.com>
2023-03-10 23:25:58 +0530

    Update PropertiesReader.java

--- src/main/java/com/hipolabs/universities/properties/PropertiesReader.java
+++ src/main/java/com/hipolabs/universities/properties/PropertiesReader.java
@@ -14,4 +14,19 @@ public class PropertiesReader {
 		}
 		return appProps;
 	}
+public class DumpStackTraceDemo 
+{ 
+    public static void main(String[] args) {
+        methodA(); 
+    } 
+
+    public static void methodA() {
+        try {
+            int num1 = 5/0; // java.lang.ArithmeticException: divide by zero
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 }
